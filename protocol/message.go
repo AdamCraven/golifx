ea1ac60bb8d023a59df32af2ebfa229f4619b488
Adam Craven <git@adamcraven.me>
2018-09-21 16:03:20 +0100

    working tests

--- protocol/message.go
+++ protocol/message.go
@@ -111,30 +111,48 @@ func EncodeBinary(h *HeaderNew) ([]byte, error) {
 		return []byte{}, err
 	}
 
-	origin := buf.Bytes()[2]
+	size := buf.Bytes()[0:2]
 	tagged := buf.Bytes()[3]
 	addressable := buf.Bytes()[4]
 	protocol := buf.Bytes()[5:7]
 	ackRequired := buf.Bytes()[19]
 	resRequired := buf.Bytes()[20]
+	source := buf.Bytes()[7:11]
+	target := buf.Bytes()[11:19] // Last two bytes always 0
+	sequence := buf.Bytes()[21]
+	_type := buf.Bytes()[22:24]
 
 	//bProtocol[1], tagged | addressable | bProtocol[0],
 	//tagged := byte(boolToUInt8(h.Header.Frame.tagged)) << 5
 	//	addressable := byte(boolToUInt8(h.Header.Frame.addressable)) << 4
 	//light.SetPower(false)
 	//fmt.Println("%v", tagged|addressable|protocol[1], h2)
+	/*
+		header := make([]byte, HeaderLength)
+		// 16bit size
+		copy(header[0:2], buf.Bytes()[0:2])
+		// 2bit origin, 1bit tagged, 1bit addressable, 4bit protocol
+		copy(header[3:4], []byte{origin<<6 | tagged<<5 | addressable<<4 | protocol[1]})
+		// 8bit protocol
+		copy(header[2:3], protocol)
+		// 32bit Source 32bit
+		copy(header[4:8], buf.Bytes()[7:11])
+		copy(header[22:23], []byte{ackRequired<<1 | resRequired})
+		copy(header[32:34], buf.Bytes()[22:24])*/
 
-	header := make([]byte, HeaderLength)
-	// 16bit size
-	copy(header[0:2], buf.Bytes()[0:2])
-	// 2bit origin, 1bit tagged, 1bit addressable, 4bit protocol
-	copy(header[3:4], []byte{origin<<6 | tagged<<5 | addressable<<4 | protocol[1]})
-	// 8bit protocol
-	copy(header[2:3], protocol)
-	// 32bit Source 32bit
-	copy(header[4:8], buf.Bytes()[7:11])
-	copy(header[22:23], []byte{ackRequired<<1 | resRequired})
-	copy(header[32:34], buf.Bytes()[22:24])
+	header := []byte{
+		size[0], size[1], protocol[0], tagged<<5 | addressable<<4 | protocol[1],
+		source[0], source[1], source[2], source[3],
+		// Target d0:73:d5:24:5e:e0
+		target[0], target[1], target[2], target[3], // target 4 bytes
+		target[4], target[5], target[6], target[7], // target
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, ackRequired<<1 | resRequired, sequence, // reserved 2 bytes, [6bits reserved, ack, res], sequence
+		// Protocol Header
+		0x00, 0x00, 0x00, 0x00, // reserved
+		0x00, 0x00, 0x00, 0x00, // reserved
+		_type[0], _type[1], 0x00, 0x00, // type 2 bytes, 2 bytes reserved
+	}
 
 	return header, nil
 }
--- protocol/message_test.go
+++ protocol/message_test.go
@@ -5,13 +5,16 @@ import (
 	"testing"
 )
 
-func TestEncode(t *testing.T) {
+func TestEncodeHeaderOnly(t *testing.T) {
 	expect := []byte{
-		0x24, 0x00, 0x00, 0x34,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0x03, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0x02, 0, 0, 0, 0,
+		// Frame
+		0x24, 0x00, 0x00, 0x34, 0xFF, 0xFF, 0xFF, 0xFF,
+		// Frame Address
+		0xd0, 0x73, 0xd5, 0x00, 0xf9, 0x14, 0x00, 0x00,
+		0, 0, 0, 0, 0, 0, 0x03, 0xFE,
+		// Protocol Header
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0x02, 0, 0, 0,
 	}
 
 	//23,33
@@ -29,31 +32,67 @@ func TestEncode(t *testing.T) {
 		addressable: true,
 		protocol:    1024,
 		source:      4294967295,
+		sequence:    254,
 		ackRequired: true,
 		resRequired: true,
-		sequence:    0,
+		target:      [8]byte{0xd0, 0x73, 0xd5, 0x00, 0xf9, 0x14, 0x00, 0x00},
 		_type:       2,
 	}
 
 	res, _ := EncodeBinary(h)
 
-	if !bytes.Equal(res[0:2], expect[0:2]) || res[0] != 36 {
-		t.Errorf("Size incorrect, got: %v, want: %v and %v", res[0:2], expect[0:2], 36)
-	}
 	if !bytes.Equal(res[3:4], expect[3:4]) {
 		t.Errorf("Protocol incorrect, got: %v, want: %v.", res[3:4], expect[3:4])
 	}
 
 	if !bytes.Equal(res[4:8], expect[4:8]) {
-		t.Errorf("Target, got: %v, want: %v.", res[4:8], expect[4:8])
+		t.Errorf("Source, got: %v, want: %v.", res[4:8], expect[4:8])
+	}
+
+	if !bytes.Equal(res[8:16], expect[8:16]) {
+		t.Errorf("Target, got: %v, want: %v.", res[8:16], expect[8:16])
 	}
 
 	if !bytes.Equal(res[22:23], expect[22:23]) {
-		t.Errorf("Ack/Res required, got: %v, want: %v.", res[22:23], expect[22:23])
+		t.Errorf("Ack/Res, got: %v, want: %v.", res[22:23], expect[22:23])
+	}
+
+	if res[24] != expect[24] {
+		t.Errorf("Sequence, got: %v, want: %v.", res[24], expect[24])
 	}
 
 	if !bytes.Equal(res[32:34], expect[32:34]) || res[32] != 2 {
 		t.Errorf("Message Type: %v, want: %v and %v.", res[32:33], expect[32:33], 2)
+	}
 
+	if !bytes.Equal(res[0:], expect[0:]) {
+		t.Errorf("Message should conver to:\n%v, want: \n%v", res[0:], expect[0:])
+	}
+
+	if len(res) != len(expect) {
+		t.Errorf("Length of header: %v, want: %v", len(res), len(expect))
+	}
+}
+
+func TestHeaderSize(t *testing.T) {
+	expect := []byte{0x24, 0x00}
+	res, _ := EncodeBinary(&HeaderNew{size: 36})
+
+	if !bytes.Equal(res[0:2], expect[0:2]) || res[0] != 36 {
+		t.Errorf("Size incorrect, got: %v, want: %v and %v", res[0:2], expect[0:2], 36)
+	}
+
+	expect = []byte{0xd0, 0x00}
+	res, _ = EncodeBinary(&HeaderNew{size: 208})
+
+	if !bytes.Equal(res[0:2], expect[0:2]) || res[0] != 208 {
+		t.Errorf("Size incorrect, got: %v, want: %v and %v", res[0:2], expect[0:2], 36)
+	}
+
+	expect = []byte{0xFF, 0x03}
+	res, _ = EncodeBinary(&HeaderNew{size: 1023})
+
+	if !bytes.Equal(res[0:2], expect[0:2]) || res[0] != 255 || res[1] != 3 {
+		t.Errorf("Size incorrect, got: %v, want: %v and %v and %v", res[0:2], expect[0:2], 255, 3)
 	}
 }
